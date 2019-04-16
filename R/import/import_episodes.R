eed7bbd088fb293b49c97b2b4d8a32ae93e3db4e
Giuliano Sposito <gsposito@ciandt.com>
2019-04-16 18:03:25 -0300

    Arquivo de importacao (e geracao das redes por cena) dos episodios.

---
+++ R/import/import_episodes.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+library(jsonlite)
+library(tidygraph)
+library(igraph)
+library(ggraph)
+
+# funcao que processa os personagens de cada cena
+processSceneCharacters <- function(listOfScenes){
+
+  # para cada cena
+  listOfScenes %>% 
+    as_tibble() %>% 
+    mutate(
+      # adiciona na scena os caracteres mapeados como um graph
+      charNetwork = map(characters, buildCharNetwork)
+    ) %>% 
+    return()
+  
+}
+
+# funcao que transforma o dataframe de personagens em uma lista
+# de arestas dos personagens
+buildCharNetwork <- function(sceneCharacters){
+  
+  # verifica se a lista de caracteres:
+  #  - nao eh vazia
+  #  - tem mais de um personagem
+  #  - tem o nome dos personagens
+  if (!is.null(sceneCharacters)){
+    if(nrow(sceneCharacters)>1) {
+      if("name" %in% names(sceneCharacters)) {
+        
+        # tranforma o data.frame de personagens
+        sceneCharNetwork <- sceneCharacters %>% 
+          select(name) %>% 
+          unlist() %>%     # pega a coluna nome e transforma num vetor
+          sort() %>%       # e ordena
+          combn(2) %>%     # combina dois a dois (arestas)
+          t() %>%          # coloca de pe e transforma em um 'from'/'to'
+          as.tibble() %>% 
+          set_names(c("from","to"))
+        
+        return(sceneCharNetwork)
+      } 
+    } 
+  } 
+  return(tibble(from=vector("character",0),to=vector("character",0)))
+}
+
+
+# importa e trata os dados dos episodios
+
+importEpisodes <- function(filename = "./data/episodes.json"){
+
+  # arquivo de episodios
+  eps_json <- fromJSON(filename)
+  
+  # adiciona alguns contadores e seleciona campos de interesse
+  eps <- eps_json$episodes %>% 
+    as.tibble() %>%
+    select(-episodeLink, -episodeAirDate, -episodeDescription, -openingSequenceLocations) %>% 
+    add_count(seasonNum, name="episodesCount") %>% 
+    mutate( scenesCount = map_int(scenes, nrow) ) %>% 
+    filter( scenesCount>0 ) %>%
+    mutate( scenes = map(scenes, processSceneCharacters) )
+  
+  return(eps)  
+}
