8e38c213f03ba5eba88ff1249f98ba711afbfaf1
Giuliano Sposito <gsposito@ciandt.com>
2019-04-16 10:50:13 -0300

    limpa e reorganiza o codigo

--- R/import/import_scenes_2.R
+++ R/import/import_scenes_2.R
@@ -4,137 +4,62 @@ library(tidygraph)
 library(igraph)
 library(ggraph)
 
-# arquivo de episodios
-eps_json <- fromJSON("./data/episodes.json")
-
-# adiciona alguns contadores e seleciona campos de interesse
-eps <- eps_json$episodes %>% 
-  as.tibble() %>%
-  add_count(seasonNum, name="episodesCount") %>% 
-  mutate(
-    scenesCount = map_int(scenes, nrow)
-  ) %>%  
-  select(-episodeLink, -episodeAirDate, 
-         -episodeDescription, -openingSequenceLocations)
-
+# funcao que processa os personagens de cada cena
 processSceneCharacters <- function(listOfScenes){
 
+  # para cada cena
   listOfScenes %>% 
     as_tibble() %>% 
     mutate(
+      # adiciona na scena os caracteres mapeados como um graph
       charNetwork = map(characters, buildCharNetwork)
     ) %>% 
     return()
   
 }
 
+# funcao que transforma o dataframe de personagens em uma lista
+# de arestas dos personagens
 buildCharNetwork <- function(sceneCharacters){
+  
+  # verifica se a lista de caracteres:
+  #  - nao eh vazia
+  #  - tem mais de um personagem
+  #  - tem o nome dos personagens
   if (!is.null(sceneCharacters)){
     if(nrow(sceneCharacters)>1) {
       if("name" %in% names(sceneCharacters)) {
-        print("ok")
-        return(tibble(from="giul",to="giul"))
-      } else {
-        print("characters has no names")
-      }
-    } else {
-      print("characters is less than 2")
-    }
-  } else {
-    print("characters is null")
-  }
-  
-  print("not ok")
-  return(tibble(from="",to=""))
-}
-
-listOfScenes <- eps[1,]$scenes[[1]] 
-sceneCharacters <- listOfScenes[5,]$characters[[1]]
-
-buildCharNetwork(sceneCharacters)
-
-
-listOfScenes %>% class()
-
-
-# extrai os personagens de uma cena criando uma rede (edges list)
-characterNetwork <- function(.scene){
-  
-  # cena sem personagens
-  if( !("characters" %in% names(.scene)) ) return(
-    tibble(
-      from="",
-      to=""
-    )
-  )
-  
-  # pega a lista de personagens
-  .scene %>% 
-    select(characters) %>% 
-    unlist() -> chars
-  
-  # se a lista tem menos que 2 pessoa, cria um dataset vazio
-  if (length(chars)<2) return(
-    tibble(
-      from="",
-      to=""
-    )
-  )
-  
-  # se tem mais de um cria uma rede combinando os caractes da cena
-  chars %>% 
-    sort() %>% 
-    combn(2) %>% 
-    t() %>% 
-    as.tibble() %>% 
-    set_names(c("from","to"))
+        
+        # tranforma o data.frame de personagens
+        sceneCharNetwork <- sceneCharacters %>% 
+          select(name) %>% 
+          unlist() %>%     # pega a coluna nome e transforma num vetor
+          sort() %>%       # e ordena
+          combn(2) %>%     # combina dois a dois (arestas)
+          t() %>%          # coloca de pe e transforma em um 'from'/'to'
+          as.tibble() %>% 
+          set_names(c("from","to"))
+        
+        return(sceneCharNetwork)
+      } 
+    } 
+  } 
+  return(tibble(from=vector("character",0),to=vector("character",0)))
 }
 
 
+# arquivo de episodios
+eps_json <- fromJSON("./data/episodes.json")
 
+# adiciona alguns contadores e seleciona campos de interesse
+eps <- eps_json$episodes %>% 
+  as.tibble() %>%
+  select(-episodeLink, -episodeAirDate, -episodeDescription, -openingSequenceLocations) %>% 
+  add_count(seasonNum, name="episodesCount") %>% 
+  mutate( scenesCount = map_int(scenes, nrow) ) %>% 
+  filter( scenesCount>0 ) %>%
+  mutate( scenes = map(scenes, processSceneCharacters) )
 
-
-
-
-
-eps %>% 
-  
-  
-  
-  
-  mutate(
-    # para cada episodio
-    void = map(scenes, function(.scenes){
-      
-      .scenes %>% 
-        class() %>% 
-        print()
-          
-    
-    })
-  )
-
-.scenes <- eps[1,]$scenes[[1]]
-
-.scenes %>% tibble()
-
-.scene <- scene[1,]
-
-.scene %>% 
-  as.tibble()
-
-eps[1,]$scenes[[1]] %>% 
-  as.tibble() -> cena
-
-cena %>% nrow()
-
-cena$characters[[20]] %>% 
-  select(name) %>% 
-  unlist() %>% 
-  combn(2) %>% 
-  t() %>% 
-  graph.edgelist(directed = FALSE) %>% 
-  as_tbl_graph() -> g
 
 # fixando o layout previamente para todos os plots terem a mesma disposicao
 g_layout <- create_layout(g, layout = "kk")
