d9fc98b979724964fccb7e93e9a97879504d82ee
Giuliano Sposito <gsposito@ciandt.com>
2019-04-17 17:34:42 -0300

    Finalizando pipe de geracao de imagem Testando layouts

--- R/pipeline/scenes_network.R
+++ R/pipeline/scenes_network.R
@@ -11,17 +11,65 @@ scenes <- extractScenes(episodes)
 # compondo as animacoes
 scenes %>% 
   select( -sceneStart ) %>% 
-  filter( sceneSequence >=10, sceneSequence < 1000 ) %>% 
+  filter( sceneSequence >= 11 ) %>% 
   mutate(
-    network = map( sceneSequence, composeNetwork, 100, 2, scenes, gotCharacters)
+    network = map( sceneSequence, composeNetwork, 100, 3, scenes, gotCharacters)
   ) -> scenesNetwork
 
-scenes$sceneSequence
+saveRDS(scenesNetwork, "./data/sceneNetwork.rds")
 
 # fixando o layout previamente para todos os plots terem a mesma disposicao
-composeNetwork(300, 100, 2, scenes, gotCharacters) %>% 
-  plotNetwork(gotCharacters)
+# composeNetwork(11, 100, 3, scenes, gotCharacters) %>% 
+#   plotNetwork(gotCharacters)
+
+scenesIndexes <- scenesNetwork$sceneSequence
 
 
-scenes %>% 
-  filter(sceneSequence==300 | sceneSequence==200)
+for(.i in scenesIndexes[1:200]){
+
+  # compoe o nome do arquivo de imagem
+  img_name <- paste0( "./images/scene_",formatC(.i, width = 4, flag="0"), ".png" )
+  
+  # abre o device de imagem
+  png(img_name)
+  
+  # plota
+  print(plotNetwork(scenesNetwork[.i,]$network[[1]], gotCharacters))
+  
+  # fecha e salva imagem
+  dev.off()
+  
+}
+
+
+images_name <- scenesIndexes %>% 
+  map_chr(function(.i, .scns, .chars){
+    
+    # compoe o nome do arquivo de imagem
+    img_name <- paste0( "./images/scene_",formatC(.i, width = 4, flag="0"), ".png" )
+    
+    # abre o device de imagem
+    png(img_name)
+    
+    # plota
+    .scns[.i,]$network[[1]] %>% 
+      plotNetwork(.chars)
+    
+    # fecha e salva imagem
+    dev.off()
+    
+    return(img_name)
+  }, scenesNetwork, gotCharacters)
+
+
+.scns[71,]$network[[1]] %>% 
+  create_layout(layout = "gem") %>% 
+  ggraph(g_layout) +
+  geom_edge_fan(aes(alpha=weight), width=1) +
+  geom_node_point(aes(size=degree, color=house), alpha=.6) +
+  geom_node_text(aes(label=name, size=degree), color="black") +
+  theme_void() +
+  scale_color_discrete(rainbow(18), labels=levels(gotCharacters$house)) +
+  theme( legend.position = "none" )
+
+
